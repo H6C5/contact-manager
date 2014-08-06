f2ea32b3ab73c2e9d4a234552a6c821b38ab0dad
Horacio Chavez <horacio.hch@gmail.com>
2014-08-05 19:45:51 -0600

    Must have a reference to a person

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PhoneNumber, :type => :model do
 
   let(:phone_number) do
-    PhoneNumber.new(number: "1112223333")
+    PhoneNumber.new(number: "1112223333", person_id: 1)
   end
 
   it 'is valid' do
@@ -13,6 +13,11 @@ RSpec.describe PhoneNumber, :type => :model do
   it 'is invalid without a number' do
     phone_number.number = nil
     expect(phone_number).to_not be_valid
+  end
+
+  it 'must have a reference to a person' do
+    phone_number.person_id = nil
+    expect(phone_number).not_to be_valid
   end
 
 end
