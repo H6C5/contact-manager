03b0a06ce07579ac6daf7c037b399bf6e7934c41
Horacio Chavez <horacio.hch@gmail.com>
2014-08-07 18:58:56 -0600

    Email Addresses connected

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -2,6 +2,6 @@ class EmailAddress < ActiveRecord::Base
 
   belongs_to :person
 
-  validates :address, presence: true
-
+  validates :address, :person_id, presence: true
+  validates :person_id, numericality: { only_integer: true }
 end
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe EmailAddress, :type => :model do
   let(:email_address) do
-    EmailAddress.new({address: "mail@example.com"})
+    EmailAddress.new({address: "mail@example.com", person_id: 1})
   end
 
   it 'is valid' do
@@ -11,6 +11,16 @@ RSpec.describe EmailAddress, :type => :model do
 
   it 'is invalid without an address' do
     email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+
+  it 'is invalid without a person_id' do
+    email_address.person_id = nil
+    expect(email_address).to_not be_valid
+  end
+
+  it 'is invalid if person_id is not an integer' do
+    email_address.person_id = "string"
     expect(email_address).to_not be_valid
   end
 
